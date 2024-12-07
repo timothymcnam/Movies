34564d0abe536bffe848b205cb81303b126bcc01
timothymcnam <timmy.mc.2@gmail.com>
2024-12-07 16:28:05 -0500

    Sample Scala Build

---
+++ src/main/scala/recs/RecsApp.scala
@@ -0,0 +1,15 @@
+package recs
+
+/**
+ * @author ${user.name}
+ */
+object RecsApp {
+  
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+  }
+
+}
---
+++ src/test/scala/recs/RecsAppTest.scala
@@ -0,0 +1,17 @@
+package recs
+
+import org.junit._
+import Assert._
+
+@Test
+class RecsAppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
